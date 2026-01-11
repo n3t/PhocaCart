9c466287b86c4cb067044877528cdf2f17abeef2
n3t <n3t@n3t.cz>
2026-01-11 23:06:27 +0100

    Conflict resolution

--- site/views/account/view.html.php
+++ site/views/account/view.html.php
@@ -8,141 +8,145 @@
  */
 defined('_JEXEC') or die();
 
-use Joomla\CMS\Form\FormField;
+use Joomla\CMS\Helper\AuthenticationHelper;
 use Joomla\CMS\MVC\View\HtmlView;
 use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 use Joomla\CMS\Form\Form;
 use Joomla\CMS\Helper\TagsHelper;
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-use Joomla\Component\Users\Administrator\Helper\Mfa;
-use Phoca\PhocaCart\MVC\View\SiteViewTrait;
+use Joomla\Registry\Registry;
+
+jimport( 'joomla.application.component.view');
 
 class PhocaCartViewAccount extends HtmlView
 {
-    use SiteViewTrait;
+	protected $t;
+	protected $r;
+	protected $p;
+	protected $u;
+	protected $s;
+	protected $fields2;
+	protected $data2;
+	protected $form2;
 
-    // Backward compatibility properties
-    protected $s;
-    protected $p;
-    protected $t;
-    protected $u;
-    protected $twofactorform = [];
-    protected $twofactormethods = [];
-    protected $otpConfig = null;
+	// User profile
+	protected $params;
+	protected $fields;
+	protected $data;
+	protected $form;
+	protected $state;
+	protected $twofactorform;
+	protected $twofactormethods;
+	protected $otpConfig;
 
-    // Phoca Cart address form
-    protected $fields2;
-    protected $data2;
-    protected $form2;
+	function display($tpl = null)
+	{
+		$app								= Factory::getApplication();
+		$this->u							= PhocacartUser::getUser();
+		$uri 								= Uri::getInstance();
+		$document							= Factory::getDocument();
+		$this->p 							= $app->getParams();
+		$this->s                            = PhocacartRenderStyle::getStyles();
+		$model								= $this->getModel();
 
-    // User profile
-    protected $params;
-    protected $fields;
-    protected $data;
-    protected $form;
-    protected $state;
+		$this->t['action']							= $uri->toString();
+		$this->t['actionbase64']					= base64_encode($this->t['action']);
+		$this->t['linkaccount']						= Route::_(PhocacartRoute::getAccountRoute());
+		$this->t['linkcheckout']					= Route::_(PhocacartRoute::getCheckoutRoute());
+		$this->t['display_edit_profile']			= $this->p->get( 'display_edit_profile', 1 );
+		$this->t['display_reward_points_total_info']= $this->p->get( 'display_reward_points_total_info', 0 );
+		$this->t['delivery_billing_same_enabled']  = $this->p->get('delivery_billing_same_enabled', 0);
 
-    protected $mfaConfigurationUI;
+		$lang = Factory::getLanguage();
+		//$lang->load('com_users.sys');
+		$lang->load('com_users');
 
-    function display($tpl = null)
-    {
-        $app = Factory::getApplication();
+		$this->t['datauser'] = [];
+		if ((int)$this->u->id > 0) {
+			// Checkout Model
+			jimport('joomla.application.component.model');
+			BaseDatabaseModel::addIncludePath(JPATH_SITE.'/components/com_phocacart/models');
+			$modelCheckout 				= BaseDatabaseModel::getInstance( 'Checkout', 'PhocaCartModel' );
 
-        $uri = Uri::getInstance();
-        // backward compatibility
-        $this->data('action', $uri->toString());
-        $this->data('actionbase64', base64_encode($this->data('action')));
-        $this->data('linkaccount', Route::_(PhocacartRoute::getAccountRoute()));
-        $this->data('linkcheckout', Route::_(PhocacartRoute::getCheckoutRoute()));
-        $this->data('display_edit_profile', $this->param('display_edit_profile', 1));
-        $this->data('display_reward_points_total_info', $this->param('display_reward_points_total_info', 0));
-        $this->data('delivery_billing_same_enabled', $this->param('delivery_billing_same_enabled', 0));
-        $this->data('datauser', []);
+			// Check if all form items are filled out by user, if yes, don't load the form and save some queries
+			$this->fields2				= $modelCheckout->getFields(0,0,1); // Fields will be loaded in every case
+			$this->data2				= $modelCheckout->getData();
+			$this->form2				= $modelCheckout->getForm();
+			$this->t['dataaddressform']	= PhocacartUser::getAddressDataForm($this->form2, $this->fields2['array'], $this->u);
+			$this->t['datauser']		= $this->data2;
 
-        $lang = $app->getLanguage();
-        $lang->load('com_users');
+			// USER PROFILE - USER MODULE
+		/*	jimport('joomla.application.component.model');
+			//JLoader::import('user',JPATH_SITE.'/components/com_users/models');
+			BaseDatabaseModel::addIncludePath(JPATH_SITE.'/components/com_users/models');
+			$modelUsers 			= BaseDatabaseModel::getInstance( 'Profile', 'UsersModel' );
 
-        if ($this->isLogged()) {
-            /** @var PhocaCartModelCheckout $modelCheckout */
-            BaseDatabaseModel::addIncludePath(JPATH_SITE . '/components/com_phocacart/models');
-            $modelCheckout = BaseDatabaseModel::getInstance('Checkout', 'PhocaCartModel');
+			$modelUsers 			=Factory::getApplication()->bootComponent('com_users')->getMVCFactory()->createModel('Profile', 'Site', ['ignore_request' => true]);
 
-            // Check if all form items are filled out by user, if yes, don't load the form and save some queries
-            $this->fields2 = $modelCheckout->getFields(0, 0, 1); // Fields will be loaded in every case
-            $this->data2   = $modelCheckout->getData();
-            $this->form2   = $modelCheckout->getForm();
 
-            $this->data('dataaddressform', PhocacartUser::getAddressDataForm($this->form2, $this->fields2['array'], $this->getUser()));
+			$this->data	            = $modelUsers->getData();
+			$loadformpath 			= JPATH_SITE.'/components/com_users';
+			Form::addFormPath($loadformpath.'/forms');
+			Form::addFieldPath($loadformpath.'/fields');
+			$this->form	  			= $modelUsers->getForm();*/
 
-            // Backward compatibility
-            $this->data('datauser', $this->data2);
 
-            $this->loadJoomlaUser();
 
-            // REWARD POINTS
-            $reward = new PhocacartReward();
-            $this->data('rewardpointstotal', $reward->getTotalPointsByUserId($this->getUser()?->id));
-        }
+			Form::addFormPath(JPATH_SITE .  '/components/com_users/forms');
+			Form::addFieldPath(JPATH_SITE .  '/components/com_users/fields');
+			$modelUsers = $app->bootComponent('com_users')->getMvcFactory()->createModel('Profile', 'Site', ['ignore_request' => false]);
 
-        $media = PhocacartRenderMedia::getInstance();
-        $media->loadBase();
-        $media->loadChosen();
+
+			//BaseDatabaseModel::addIncludePath(JPATH_SITE.'/components/com_users/models');
+			//$modelUsers 			= BaseDatabaseModel::getInstance( 'Profile', 'UsersModel' );
+
+			$this->form = $modelUsers->getForm();
+
+			$this->data	            = $modelUsers->getData();
+			$this->state            = $modelUsers->getState();
+			$this->params           = $this->state->get('params');
+			$this->twofactorform    = $modelUsers->getTwofactorform();
+			$this->twofactormethods = AuthenticationHelper::getTwoFactorMethods();
+			$this->otpConfig        = $modelUsers->getOtpConfig();
+			$this->data->tags 		= new TagsHelper;
+			$this->data->tags->getItemTags('com_users.user.', $this->data->id);
+
+
+			// REWARD POINTS
+			$reward = new PhocacartReward();
+			$this->t['rewardpointstotal'] = $reward->getTotalPointsByUserId((int)$this->u->id);
+
+
+			// SUBSCRIPTIONS
+			$this->t['subscriptions'] = PhocacartSubscription::getSubscriptions((int)$this->u->id);
+            $this->t['subscription_description_account_view'] = '';
+            // Get plugin parameters
+            $plugin = PluginHelper::getPlugin('system', 'phocacartsubscription');
+            if ($plugin && isset($plugin->params)) {
+                $params = new Registry($plugin->params);
+                $this->t['subscription_description_account_view'] = $params->get('subscription_description_account_view', '');
+                $this->t['subscription_description_account_view'] = HTMLHelper::_('content.prepare', $this->t['subscription_description_account_view']);
+            }
+		}
+
+		$media = PhocacartRenderMedia::getInstance('main');
+		$media->loadBase();
+		$media->loadChosen();
+		//- PhocacartRenderJs::renderBillingAndShippingSame();
         $media->loadSpec();
 
-        $this->_prepareDocument();
+		$this->_prepareDocument();
+		parent::display($tpl);
 
-        // Backward compatibility
-        $this->p = $this->getParams();
-        $this->s = $this->getStyles();
-        $this->t = $this->data();
-        $this->u = $this->getUser();
+	}
 
-        parent::display($tpl);
-    }
-
-    private function loadJoomlaUser(): void
-    {
-        if (!$this->param('display_edit_profile', 1)) {
-            return;
-        }
-
-        Form::addFormPath(JPATH_SITE . '/components/com_users/forms');
-        Form::addFieldPath(JPATH_SITE . '/components/com_users/fields');
-
-        $app = Factory::getApplication();
-
-        // Redirect back to Phoca Cart Account
-        $app->setUserState('com_users.edit.profile.redirect', Uri::getInstance()->toString());
-
-        /** @var \Joomla\Component\Users\Site\Model\ProfileModel $modelUsers */
-        $modelUsers = $app->bootComponent('com_users')->getMvcFactory()->createModel('Profile', 'Site', ['ignore_request' => false]);
-
-        $this->form = $modelUsers->getForm();
-
-        $this->data   = $modelUsers->getData();
-        $this->state  = $modelUsers->getState();
-        $this->params = $this->state->get('params');
-
-        $this->mfaConfigurationUI = Mfa::getConfigurationInterface($this->getcurrentUser());
-
-        $this->data->tags = new TagsHelper;
-        $this->data->tags->getItemTags('com_users.user.', $this->data->id);
-    }
-
-    protected function _prepareDocument()
-    {
-        PhocacartRenderFront::prepareDocument($this->getDocument(), $this->getParams(), false, false, Text::_('COM_PHOCACART_ACCOUNT'));
-    }
-
-    public function renderField(FormField $field): string
-    {
-        $return = str_replace('form-control', $this->classMap('inputbox.form-control'), $field->renderField());
-        $return = str_replace('form-select', $this->classMap('inputbox.form-select'), $return);
-        $return = str_replace('btn btn-secondary', $this->classMap('btn.btn-secondary'), $return);
-
-        return $return;
-    }
+	protected function _prepareDocument() {
+		PhocacartRenderFront::prepareDocument($this->document, $this->p, false, false, Text::_('COM_PHOCACART_ACCOUNT'));
+	}
 }
+?>
